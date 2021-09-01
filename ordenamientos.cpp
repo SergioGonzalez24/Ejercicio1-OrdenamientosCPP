0e5bdc1063c0ad1ba07d63d44004265e7982ab31
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-09-01 15:43:58 -0500

    agragar metodos de intercambio

--- ordenamientos.cpp
+++ ordenamientos.cpp
@@ -9,7 +9,7 @@ using namespace std;
 //Dos lugares para almacenar datos Stack y el Heap
 
 //Generar arreglos aleatorios
-void generarArreglo(int arreglo [],int tam){
+void generarArreglo(int arreglo [],int tam) {
     //inicializar la semilla de generacion de numeros aleatorios
     srand((unsigned)time(0));
     for(int i=0;i<tam;i++)
@@ -17,13 +17,28 @@ void generarArreglo(int arreglo [],int tam){
 }
 
 //Imprimir un arreglo
-void imprimirArreglo(int arreglo [], int tam){
+void imprimirArreglo(int arreglo [], int tam) {
     for(int i=0;i<tam;i++)
         cout<< arreglo[i]<<" ";
     cout<<endl;
 }
 
-int main(/*Parametros de entrada*/){
+void metodoIntercambio(int arreglo [], int tam) {
+    int i,j,aux;
+    for (i=0;i<=tam-1;i++) { //Va a ser para las pasadas, tiene control de indice fijo
+        for (j=1+1;j<tam;j++) { //Indice de comparracion
+            if (arreglo[i]>arreglo[j]) { //validar que el elemento de comparacion es mayor a nuestro indice fijo
+                aux = arreglo[j];
+                arreglo[j] = arreglo[i];
+                arreglo[i] = aux;
+
+            }
+
+        }
+    }
+}
+
+int main(/*Parametros de entrada*/) {
     int tam;
     cout<<"Ingrese el tamaño del arreglo: ";
     cin>>tam;
