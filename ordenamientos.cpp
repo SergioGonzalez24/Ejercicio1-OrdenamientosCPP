97fd655695df0a443a6b11badb88512416db0401
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2021-12-02 22:57:25 -0600

    .

--- ordenamientos.cpp
+++ ordenamientos.cpp
@@ -23,6 +23,8 @@ void imprimirArreglo(int arreglo [], int tam) {
     cout<<endl;
 }
 
+//METODOS DE ORDENAMIENTOS BASICOS
+///////////////////////////////////////////////////////////////////
 void metodoIntercambio(int arreglo [], int tam) {
     int i,j,aux;
     for (i=0;i<=tam-1;i++) { //Va a ser para las pasadas, tiene control de indice fijo
@@ -54,16 +56,91 @@ void metodoBurbuja(int arreglo [], int tam) {
         }
     }
 }
+//***********************************************************
+void metodoSeleccionTarea(int arreglo [], int tam) { //TAREA
 
-void metodoSeleccion(int arreglo [], int tam) { //TAREA
     int i,j,aux;
-    for (i=0;i<=tam-1;i++) {
-    
+    for (i=0; i<tam;i++){
+        for (j = 0; j < tam;j++){
+            if (arreglo[i]<arreglo[j])
+            {
+                 aux = arreglo[j];
+                 arreglo[j]=arreglo[i];
+                 arreglo[i]=aux;
+
+            }
+        }
     }
+}
+//***********************************************************
+
+
+void metodoSeleccion(int arreglo [], int tam) { 
+    int pasada,j,indicemenor,aux;
+    for(pasada=0;pasada<tam-1;pasada++) {
+        indicemenor=pasada;
+        //Busqueda del indice del menor valor
+        for(j=indicemenor+1;j<tam;j++) {
+            if(arreglo[j]<arreglo[indicemenor]) {
+                indicemenor=j; //Actualizar el indice del elemento menor actual
+            }
+        }
+        //Swap o intercambio
+        aux=arreglo[pasada];
+        arreglo[pasada]=arreglo[indicemenor];
+
+    }
+}
+
+
+void metodoInsercion(int arreglo [], int tam) {
+    int i,j,aux;
+    for(i=1;i<tam;i++) {
+        j=i;
+        aux=arreglo[i];
+        while(j>0&&aux<arreglo[j-1]) { //Rcorrrer elementos mayores a la derecha
+            arreglo[j]=arreglo[j-1];
+            j--;
+        }
+        arreglo[j]=aux;
+    }
+}
+////////////////////////////////////////////////////////////////////////
+
+//METODOS DE ORDENAMIENTO AVANZADOS
+void merge(int arreglo[], int inicio, int mitad, int final) {
+    //N+numero de elementos e la parte izq 
+    //Numero de elementos en la parte derecha
+    //Declarar mis arreglos auxiliares de izq a derecha
+    //Copiar los datos del arreglo originale en izq y der
+    //Realizar el merge
+    //vaciar rematentes
 
 }
 
+void mergeSort(int arreglo [], int inicio, int final) {
+    if (inicio<final) { //Caso recursivo
+        //obtener el indice medio
+        int mitad=inicio+(final-inicio)/2;
+        //Parte Izquierda
+        mergeSort(arreglo,inicio,mitad);
+        //Parte Final
+        mergeSort(arreglo,inicio+1,final);
+    } 
+    //Caso base
+    else {
+        merge(arreglo,inicio,mitad,fin);
+    }
+
+
+}
+
+
+
+
+
 int main(/*Parametros de entrada*/) {
+
     int tam;
     cout<<"Ingrese el tamaño del arreglo: ";
     cin>>tam;
@@ -84,4 +161,9 @@ int main(/*Parametros de entrada*/) {
     cout << "Metodo Burbuja" << endl;
     metodoBurbuja(arreglo,tam);
     imprimirArreglo(arreglo,tam);
+
+    cout << "Metodo Seleccion" << endl;
+    metodoSeleccionTarea(arreglo,tam);
+    imprimirArreglo(arreglo,tam);
+
 }
